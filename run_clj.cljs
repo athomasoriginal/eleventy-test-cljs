6da108621b6c3c7735df296b07acbc88bec04ce5
athomasoriginal <thomasmattacchione@gmail.com>
2023-11-22 16:48:36 -0500

    refactor: simplify `run_clj` - apply solution only
    Prior to this commit, I was unsure what the issue was.  Seems the issue
    is that the `src.extra.components` ns was not reloading.  The change you
    see here forces the reload to happen.

--- run_clj.cljs
+++ run_clj.cljs
@@ -12,15 +12,11 @@
 
 (defn compile-file
   [file-name]
-  (p/let [components-res (nbb/slurp "./src/extra/components.cljs")]
-    (-> (nbb/load-string components-res)
-        (.then (fn [result]
-                 (js/console.log "SLURP TEST 1: " (rds/render-to-static-markup [(last-form result)]))
-                 (js/console.log "SLURP TEST 2: " (rds/render-to-static-markup [(last-form result)]))))))
-  (-> (nbb/load-file file-name)
-      (.then (fn [result]
-               (rds/render-to-static-markup (last-form result))))))
+  (p/let [;; @note manually reload deps or they won't be picked up on change
+          dep-a         (nbb/slurp "./src/extra/components.cljs")
+          result-dep-a  (nbb/load-string dep-a)
+          result        (nbb/load-file file-name)]
+    (rds/render-to-static-markup (last-form result))))
 
-
-(p/let [resp (compile-file "./src/test.cljs")]
-  resp)
+(nbb/await
+  (compile-file "./src/test.cljs"))
